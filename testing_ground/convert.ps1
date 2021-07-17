902ba0a55f8be67e6ef61cb6b977dc18cc0b74a4
Mr-645 <18206187+Mr-645@users.noreply.github.com>
2021-07-17 14:28:13 +1200

    Delete testing_ground directory

--- testing_ground/convert.ps1
+++
@@ -1,9 +0,0 @@
-# Version 1
-
-<#
-Write-Output "The current directory is $(Get-Location)"
-Write-Output "The curent date and time is $(Get-Date -UFormat "%T-%A-%d-%B-%Y")"
-Write-Output "The curent date and time is $(Get-Date -UFormat "%H%M-%A-%d-%B-%Y")"
-#>
-
-Invoke-Expression -Command "C:\Users\drift\AppData\Local\Programs\Python\Python39\Scripts\pyuic5.exe -x 'app.ui' -o 'app_$(Get-Date -UFormat "%H%M-%A-%d-%B-%Y").py'"
